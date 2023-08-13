c39cf08fd5a9f94b17235e99995337795b68ad55
MNK <44355631+raleev@users.noreply.github.com>
2023-08-13 16:11:21 -0400

    Update colorscheme.lua

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -1,6 +1,7 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+local colorscheme = "catppuccin"
+local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status then
   print("Colorscheme not found!") -- print error if colorscheme not installed
   return
